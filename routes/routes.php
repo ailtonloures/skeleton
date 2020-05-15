1e93a249aa3cc75d912ed1c794a48e69131bdec1
ailtonloures <ailton.loures99@gmail.com>
2020-05-14 21:29:04 -0300

    retirando rota de teste

--- routes/routes.php
+++ routes/routes.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 /** @var \App\Services\Slim $router */
 
 $router->get('/', Controller::class .':index')->setName('index');
-$router->get('/teste', Controller::class . ':teste')->setName('teste');
 
 $router->group('/oauth', function() use ($router) {
     $router->get('/google', AuthController::class . ':googleLogin');
