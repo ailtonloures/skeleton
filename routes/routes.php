f4f67788326c39795a9b60a1e6f64d03a0e6897c
Ailton Loures <ailton.loures99@gmail.com>
2020-05-17 14:56:05 -0300

    retirando setName

--- routes/routes.php
+++ routes/routes.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Controller;
 
 /** @var \App\Services\Slim $router */
 
-$router->get('/', Controller::class .':index')->setName('index');
+$router->get('/', Controller::class .':index');
 
 $router->group('/oauth', function() use ($router) {
     $router->get('/google', AuthController::class . ':googleLogin');
