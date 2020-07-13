79fcd35fbf5153fc35351659fc419cee19d95b0a
Ailton Loures <ailton.loures99@gmail.com>
2020-07-13 14:25:06 -0300

    feat: criando duas forma de minificacao

--- commands/minify.php
+++ commands/minify.php
@@ -3,34 +3,67 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 try {
-    // CSS Minify
-    $cssDirName = dirname(__DIR__, 1) . '/templates/assets/css/';
-    $cssDir     = dir($cssDirName);
-    $minCSS     = new \MatthiasMullie\Minify\CSS();
 
-    while (($file = $cssDir->read()) != false) {
-        $cssFile = $cssDirName . $file;
+    $argument = $argv[1]; // all-for-one or one-for-one
 
-        if (is_file($cssFile) && pathinfo($cssFile)["extension"] == "css") {
-            $minCSS->add($cssFile);
+    if (isset($argument)) {
+        // Path for CSS
+        $cssDirName = dirname(__DIR__, 1) . '/templates/assets/css/';
+        $cssDir     = dir($cssDirName);
+        // Path for JS
+        $jsDirName = dirname(__DIR__, 1) . '/templates/assets/js/';
+        $jsDir     = dir($jsDirName);
+
+        if ($argument == "all-for-one") {
+
+            $minCSS = new \MatthiasMullie\Minify\CSS();
+            $minJS  = new \MatthiasMullie\Minify\JS();
+
+            // CSS Minify
+            while (($file = $cssDir->read()) != false) {
+                $cssFile = $cssDirName . $file;
+
+                if (is_file($cssFile) && pathinfo($cssFile)["extension"] == "css") {
+                    $minCSS->add($cssFile);
+                }
+            }
+
+            // JS Minify
+            while (($file = $jsDir->read()) != false) {
+                $jsFile = $jsDirName . $file;
+
+                if (is_file($jsFile) && pathinfo($jsFile)["extension"] == "js") {
+                    $minJS->add($jsFile);
+                }
+            }
+
+            $minCSS->minify(dirname(__DIR__, 1) . '/public/assets/style.min.css');
+            $minJS->minify(dirname(__DIR__, 1) . '/public/assets/index-min.js');
+
+        } else if ($argument == "one-for-one") {
+            // CSS Minify
+            while (($file = $cssDir->read()) != false) {
+                $cssFile = $cssDirName . $file;
+
+                if (is_file($cssFile) && pathinfo($cssFile)["extension"] == "css") {
+                    $minCSS = new \MatthiasMullie\Minify\CSS();
+                    $minCSS->add($cssFile);
+                    $minCSS->minify(dirname(__DIR__, 1) . '/public/assets/' . pathinfo($cssFile)["filename"] . '.min.css');
+                }
+            }
+
+            // JS Minify
+            while (($file = $jsDir->read()) != false) {
+                $jsFile = $jsDirName . $file;
+
+                if (is_file($jsFile) && pathinfo($jsFile)["extension"] == "js") {
+                    $minJS = new \MatthiasMullie\Minify\JS();
+                    $minJS->add($jsFile);
+                    $minJS->minify(dirname(__DIR__, 1) . '/public/assets/' . pathinfo($jsFile)["filename"] . '-min.js');
+                }
+            }
         }
     }
-
-    // JS Minify
-    $jsDirName = dirname(__DIR__, 1) . '/templates/assets/js/';
-    $jsDir     = dir($jsDirName);
-    $minJS     = new \MatthiasMullie\Minify\JS();
-
-    while (($file = $jsDir->read()) != false) {
-        $jsFile = $jsDirName . $file;
-
-        if (is_file($jsFile) && pathinfo($jsFile)["extension"] == "js") {
-            $minJS->add($jsFile);
-        }
-    }
-
-    $minCSS->minify(dirname(__DIR__, 1) . '/public/assets/style.min.css');
-    $minJS->minify(dirname(__DIR__, 1) . '/public/assets/index-min.js');
 
     echo "...\n\nAll files were minified and saved to public/assets\n\n...";
 
