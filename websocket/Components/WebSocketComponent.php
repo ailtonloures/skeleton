f364675fc87adbcdb4d4715b06eb6f05502dac08
ailtonloures <ailton.loures99@gmail.com>
2021-04-10 00:51:26 -0300

    adicionando funcionalidade para websocket

---
+++ websocket/Components/WebSocketComponent.php
@@ -0,0 +1,49 @@
+<?php
+namespace WebSocket\Components;
+
+use Exception;
+use Ratchet\RFC6455\Messaging\MessageInterface;
+use Ratchet\WebSocket\MessageComponentInterface;
+use React\Socket\ConnectionInterface;
+use SplObjectStorage;
+
+class WebSocketComponent implements MessageComponentInterface
+{
+    /** @var SplObjectStorage $connections */
+    private $connections;
+
+    public function __construct()
+    {
+        $this->connections = new SplObjectStorage();
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        echo "Connected!\n";
+
+        $this->connections->attach($conn);
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        echo "Connection closed\n";
+
+        $this->connections->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, Exception $e)
+    {
+        echo "Error: {$e->getTraceAsString()}";
+
+        $this->connections->detach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, MessageInterface $msg)
+    {
+        foreach ($this->connections as $conn) {
+            if ($conn !== $from) {
+                $conn->send((string) $msg);
+            }
+        }
+    }
+}
